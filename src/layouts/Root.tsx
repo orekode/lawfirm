8943fa4215eda9d6c87acdab2d3267e084f839fa
orekode <shalommckdavid@gmail.com>
2024-01-18 16:20:35 +0000

    add listener to rootlayout to ensure page scrolls to the top on page change

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useEffect } from 'react';
 
 AOS.init();
 const Root = () => {
 
+  const location = useLocation();
+
+
+  useEffect(() => window.scrollTo(0,0), [location]);
+
   setInterval(() => {
     localStorage.removeItem('ghhg');
-  }, 1000 * 60 * 20) //20 mins
+  }, 1000 * 60 * 20); //20 mins
+  
   return (
     <div>
         <Outlet />
